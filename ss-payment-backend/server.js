fd3b4ffc82f6876249e50df91ec9b3546111afff
KHUSHANT WANKHEDE <yashwankhede53@gmail.com>
2026-06-29 17:55:21 +0530

    added cors origin

--- ss-payment-backend/server.js
+++ ss-payment-backend/server.js
@@ -24,6 +24,7 @@ app.use(
         'http://localhost:5174',
         'http://127.0.0.1:5174',
         'https://ss-payment-portal.vercel.app',
+        'https://payment-ss.vercel.app'
       ];
 
       if (!origin || allowedOrigins.includes(origin) || /^http:\/\/localhost:\d+$/i.test(origin) || /^http:\/\/127\.0\.0\.1:\d+$/i.test(origin)) {
